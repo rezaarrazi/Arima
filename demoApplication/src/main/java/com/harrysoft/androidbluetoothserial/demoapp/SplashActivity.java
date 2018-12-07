e1060e91e107a70a0df10b9768c480a164bbe1c4
FAIQ-ROG <karyoutomoo@gmail.com>
2018-12-07 15:32:02 +0700

    UI Rework
    Splash, colors, etc

---
+++ demoApplication/src/main/java/com/harrysoft/androidbluetoothserial/demoapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.harrysoft.androidbluetoothserial.demoapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+    }
+}
